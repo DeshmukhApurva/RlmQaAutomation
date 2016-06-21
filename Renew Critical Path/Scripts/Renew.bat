376646f698536f6aeeef96cc39a0bbfea27b1683
cpchouksey <cp.chouksey@gmail.com>
2016-06-21 12:38:32 +0530

    Updated credentials

--- Renew Critical Path/Scripts/Renew.bat
+++ Renew Critical Path/Scripts/Renew.bat
@@ -1,6 +1,4 @@
+rem navigate to the directory where the batch file and scripts exists
 cd /d %~dp0
 
-rem Run cucumber
-rem cucumber ../Features -r ../Features --format html --out %_filename% --tags @%_tag% UserRole='%_role%' Browser='%_browser%' --guess
-
-start cmd /k call Renew_Critical_Path chrome RenWithinFiscalYear RENEW_EETEST4
+start cmd /k call Renew_Critical_Path chrome valid_renewal_relationship RENEWNETOPS_EETEST4
