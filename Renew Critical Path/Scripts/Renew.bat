d66281f9982dbf8bbba6b7a22da2bb8d20946455
fpriyadarshi <fpriyadarshi@SSA01.SERVICESOURCE.com>
2016-07-15 14:15:35 +0000

    tet

--- Renew Critical Path/Scripts/Renew.bat
+++ Renew Critical Path/Scripts/Renew.bat
@@ -2,4 +2,4 @@ rem navigate to the directory where the batch file and scripts exists
 cd /d %~dp0
 
 
-start cmd /k call Renew_Critical_Path firefox update_opportunity_assert_fields RENEWNETOPS_EETEST4
+start cmd /k call Renew_Critical_Path firefox view_Contract_line_item RENEWNETOPS_EETEST4
