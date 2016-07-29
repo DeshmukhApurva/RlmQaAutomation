4a676baca8dae206d99d07a58ccf35d0685cc196
Apurva <apurvad@comitydesigns.com>
2016-07-29 11:14:15 +0530

    Added Steps and its Step Defination

--- Renew Critical Path/Features/MasterDataCreation.feature
+++ Renew Critical Path/Features/MasterDataCreation.feature
@@ -6,7 +6,7 @@ Background: Login to the Application
 	Then I should land on CRM home page
 
 @Createnewaccount
-Scenario: Renew: New account creation
+Scenario: Rally Test Case ID:  : Renew: New account creation
    	When I click on "Account" tab
 	Then I create new Renew Account with value "PE Wealth"
 
@@ -16,45 +16,51 @@ Scenario: Renew: New account creation
 #	Then I create a SuccessPlan with template "APT-000019" with Account "PE Wealth"
 
 @CreateAssetsRef
-Scenario: Renew: Asset creation
+Scenario: Rally Test Case ID:  : Renew: Asset creation
   When I click on "Asset" tab
   Then I create new Asset with Name "PEW Asset" and its Status "Installed" 
   
 @CreateAsset
-Scenario: Renew: Asset creation
+Scenario: Rally Test Case ID:  : Renew: Asset creation
   When I click on "Asset" tab
   Then I create new Asset with Name "CA_CP_Asset" and its Product "Product A" 
   When I click on "Asset" tab 
   Then I create new Asset with Name "PESAsset" and its Product "Product A"
   
 @CreateServiceContracts
-Scenario: Renew: Asset creation
+Scenario: Rally Test Case ID:  : Renew: Asset creation
   When I click on "Service Contracts" tab
   Then I create new Service Contracts with Name "PEW SC" with Account "PE Wealth" 
 		
 @CreateSC
-Scenario: Renew: Service Contract creation
+Scenario: Rally Test Case ID:  : Renew: Service Contract creation
   When I click on "Service Contracts" tab
-  Then I create new Service Contracts with Name "RenewNetOpsSCTest" with Account "QA Test" 
-
+  Then I create new Service Contracts with Name "RenewNetOpsSCTest" with Account "QA Test"
+  When I click on "Service Contracts" tab 
+  Then I select the first Contract with its Product "Product A"
+   
 @ServiceContractsCreation
- Scenario: Renew: Service Contract creation
+ Scenario: Rally Test Case ID:  : Renew: Service Contract creation
   When I click on "Service Contracts" tab
-  Then I create new Service Contracts with Name "PEW SC" with Account "QA Test" with Product "Product B" with ContractName "SourceServiceContract" 
+  Then I create new Service Contracts with Name "SourceServiceContract" with Account "QA Test"
+  When I click on "Service Contracts" tab
+  Then I select with its Product "Product B"
  
 @CreateCustomPriceBook
-Scenario: Renew: CustomPriceBook creation
+Scenario: Rally Test Case ID:  : Renew: CustomPriceBook creation
   Then I create new custom pricebook
 
- 
 @CreatemasterdataforAddOnRenewalOpp
 Scenario: Renew: Add on Renewals creation
   When I click on "Opportunities" tab
   Then I create new opportunity with Name "SourceOpportunity" with Account "QA Test"
-  Then I add opportunity product "Product A"
+  When I click on "Opportunities" tab
+  Then I select with its Product "Product B"
   When I click on "Service Contracts" tab
   Then I create new Service Contracts with Name "RenewNetOpsService" with Account "QA Test"
-  Then I add contract line item "Product B"
+  When I click on "Service Contracts" tab
+  Then I select with its Product "Product B"
+ 
 #  When I click on "Opportunities" tab
 #  Then I create new opportunity with Name "RenewNetOpsService" with Account "QA Test"
 #  Then I add opportunity product "Product C"
@@ -66,4 +72,5 @@ Scenario: Renew: Generate Opp
  Then I add opportunity products
  
 
+ 
   
--- Renew Critical Path/Features/step_definitions/MasterDataCreation.rb
+++ Renew Critical Path/Features/step_definitions/MasterDataCreation.rb
@@ -10,11 +10,11 @@ When(/^I click on "([^"]*)" tab$/) do |tab|
   rescue Exception => ex
     putstr "Error occurred while clicking the #{tab} tab"
     putstr_withScreen  ex.message
- end
+  end
 end
 
 Then(/^I create new Renew Account with value "(.*?)"$/) do |arg|
-  begin 
+  begin
     click_on "New"
     sleep 2
     puts "Creating a new Account"
@@ -26,8 +26,8 @@ Then(/^I create new Renew Account with value "(.*?)"$/) do |arg|
       click_on "Save"
     end
     sleep 5
-    puts "Account  successfully created" 
-    rescue Exception => ex
+    puts "Account  successfully created"
+  rescue Exception => ex
     puts "Error occurred while creating Account"
     puts ex.message
   end
@@ -47,15 +47,38 @@ Then(/^I create new Asset with Name "(.*?)" and its Status "(.*?)"$/) do |arg1,a
     within(:id,"topButtonRow") do
       click_on "Save"
     end
-    
-    puts "Assest  successfully created" 
-    rescue Exception => ex
+
+    puts "Assest  successfully created"
+  rescue Exception => ex
     puts "Error occurred while creating Assest"
     puts ex.message
   end
-end    
+end
 
-Then(/^I create new Service Contracts with Name "(.*?)" with Account "(.*?)"^$/) do |arg1,arg2|  
+Then(/^I create new Asset with Name "(.*?)" and its Product "(.*?)"$/) do |arg1,arg2|
+  begin
+    puts "Inside Method"
+    click_on "New"
+    sleep 2
+    puts "Creating a new Asset"
+    sleep 4
+    fill_in "Name",:with => arg1
+    puts "Filled value for Assest name"
+    sleep 5
+    fill_in "Product2",:with =>arg2
+    sleep 5
+    within(:id,"topButtonRow") do
+      click_on "Save"
+    end
+
+    puts "Assest  successfully created"
+  rescue Exception => ex
+    puts "Error occurred while creating Assest"
+    puts ex.message
+  end
+end
+
+Then(/^I create new Service Contracts with Name "(.*?)" with Account "(.*?)"$/) do |arg1,arg2|
   begin
     click_on "New"
     sleep 2
@@ -70,34 +93,69 @@ Then(/^I create new Service Contracts with Name "(.*?)" with Account "(.*?)"^$/)
     within(:id,"topButtonRow") do
       click_on "Save"
     end
-    
-    puts "Service Contract  successfully created" 
-    rescue Exception => ex
+    sleep 10
+    puts "Service Contract  successfully created"
+  rescue Exception => ex
     puts "Error occurred while creating Service Contract"
     puts ex.message
   end
-end 
-
-Then(/^I create new Asset with Name "(.*?)" and its Product "(.*?)"$/) do |arg1,arg2|
+end
+Then (/^I select with its Product "(.*?)"$/) do |arg|
   begin
-     puts "Inside Method"
-     click_on "New"
-    sleep 2
-    puts "Creating a new Asset"
-    sleep 4
-    fill_in "Name",:with => arg1
-    puts "Filled value for Assest name"
+    ProductName=arg
+    puts "ProductName"+ProductName
     sleep 5
-    fill_in "Product2",:with =>arg2
-    sleep 5
-    within(:id,"topButtonRow") do
-      click_on "Save"
+    within('.list') do
+      first(:link).click
     end
-    
-    puts "Assest  successfully created" 
-    rescue Exception => ex
-    puts "Error occurred while creating Assest"
-    puts ex.message
-  end
- end 
+    sleep 4
+    click_on "Add Line Item"
+    puts "Successfully click on  Add Line Items"
+    sleep 5
+    first(:xpath, "//*[contains(@id, 'p1')]").select("Standard Price Book")
+    sleep 5
 
+    click_on "Save"
+    puts "Select Price book"
+
+    within (".x-grid3-body") do
+      tr=all(".x-grid3-row")
+      tr.each do |row|
+        puts row
+        puts row.all("td")[1].text
+        if row.all("td")[1].text == ProductName
+          row.all("td")[0].first("div").first("input").click
+        end
+      end
+    end
+    click_on 'Select'
+    puts "Successfully select the product"
+    sleep 8
+
+  rescue Exception => ex
+    putstr "Error occurred while opening a SuccessPlan"
+    putstr_withScreen ex.message
+  end
+end
+
+Then(/^I create new opportunity with Name "(.*?)" with Account "(.*?)"$/) do |arg1,arg2|
+  begin
+    within (".pbHeader") do
+      click_on "New"
+    end
+    puts "New Opportunity button clicked."
+    sleep 5
+    fill_in "oop3",:with => arg1
+    puts "Filling name"
+    sleep 8
+    fill_in "oop4",:with => arg2
+    puts "Account is Selected successfully"
+    sleep 8
+    time = Time.new
+    fill_in "Close Date",:with => time.month.to_s + "/" + time.day.to_s + "/" +time.year.to_s
+    puts "Date is filled in Close Date"
+    sleep 5
+    select "Qualification", :from => "Stage"
+    puts "Select Stage"
+  end
+end      
