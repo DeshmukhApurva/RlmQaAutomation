3b451e235cdab42908ac6fe845e461a5c61c6ddf
Fagun <fagun@comitydesigns.com>
2016-08-16 14:16:59 +0530

    Updated code for US1420

--- Renew Critical Path/Scripts/Renew.bat
+++ Renew Critical Path/Scripts/Renew.bat
@@ -1,9 +1,4 @@
 rem navigate to the directory where the batch file and scripts exists
 cd /d %~dp0
 
-
-<<<<<<< HEAD
-start cmd /k call Renew_Critical_Path firefox view_Contract_line_item RENEWNETOPS_EETEST4
-=======
-start cmd /k call Renew_Critical_Path firefox Opportunity RENEW_EETEST4
->>>>>>> refs/remotes/origin/612_v1.36.8
+start cmd /k call Renew_Critical_Path firefox SplitOppLineItemNewClonedOppFieldsAsPerFieldSet RENEW_20
